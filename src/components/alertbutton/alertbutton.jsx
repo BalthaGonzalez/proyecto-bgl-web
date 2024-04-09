5ca710a2e3efd1e755facc9d57da2fad7c02671d
Balthazar Gonzalez <balthagonzalez24@gmail.com>
2024-04-09 19:53:33 -0300

    Feat: pages changes

--- src/components/alertbutton/alertbutton.jsx
+++ src/components/alertbutton/alertbutton.jsx
@@ -11,7 +11,7 @@ export default function Alertbutton() {
 
         <a href="../src/assets/Curriculum-Vitae.pdf" target="_blank" rel="noopener noreferrer">
             <AlertButton message="Visualizar Curriculum Vitae">
-                <button className="m-1 relative h-12 overflow-hidden rounded bg-neutral-950 px-5 py-2.5 text-white transition-all duration-300 hover:bg-neutral-800 hover:ring-2 hover:ring-neutral-800 hover:ring-offset-2">
+                <button className="m-1 relative h-12 overflow-hidden rounded bg-neutral-950 px-5 py-2.5 text-white transition-all duration-300 hover:ring-2 hover:ring-neutral-800 hover:ring-offset-2">
                     <span className="relative">CV</span>
                 </button>
             </AlertButton>
--- src/components/menu/menu.jsx
+++ src/components/menu/menu.jsx
@@ -5,15 +5,15 @@ export default function Menu() {
     return (
         <nav>
             <NavLink to="/">
-                <button class="mx-4 relative after:absolute after:bottom-0 after:left-0 after:right-0 after:h-[1px] after:w-full after:origin-bottom after:scale-x-0 after:transition-transform after:duration-300 after:ease-[cubic-bezier(0.65_0.05_0.36_1)] hover:after:origin-bottom hover:after:scale-x-100">
+                <button className="mx-4 relative after:absolute after:bottom-0 after:left-0 after:right-0 after:h-[1px] after:w-full after:origin-bottom after:scale-x-0 after:transition-transform after:duration-300 after:ease-[cubic-bezier(0.65_0.05_0.36_1)] hover:after:origin-bottom hover:after:scale-x-100">
                     Home</button>
             </NavLink>
             <NavLink to="cv">
-                <button class="mx-4 relative after:absolute after:bottom-0 after:left-0 after:right-0 after:h-[1px] after:w-full after:origin-bottom after:scale-x-0 after:transition-transform after:duration-300 after:ease-[cubic-bezier(0.65_0.05_0.36_1)] hover:after:origin-bottom hover:after:scale-x-100">
+                <button className="mx-4 relative after:absolute after:bottom-0 after:left-0 after:right-0 after:h-[1px] after:w-full after:origin-bottom after:scale-x-0 after:transition-transform after:duration-300 after:ease-[cubic-bezier(0.65_0.05_0.36_1)] hover:after:origin-bottom hover:after:scale-x-100">
                     Curriculum Vitae</button>
             </NavLink>
             <NavLink to="portafolio">
-                <button class="mx-4 relative after:absolute after:bottom-0 after:left-0 after:right-0 after:h-[1px] after:w-full after:origin-bottom after:scale-x-0 after:transition-transform after:duration-300 after:ease-[cubic-bezier(0.65_0.05_0.36_1)] hover:after:origin-bottom hover:after:scale-x-100">
+                <button className="mx-4 relative after:absolute after:bottom-0 after:left-0 after:right-0 after:h-[1px] after:w-full after:origin-bottom after:scale-x-0 after:transition-transform after:duration-300 after:ease-[cubic-bezier(0.65_0.05_0.36_1)] hover:after:origin-bottom hover:after:scale-x-100">
                     Portafolio</button>
             </NavLink>
         </nav>
