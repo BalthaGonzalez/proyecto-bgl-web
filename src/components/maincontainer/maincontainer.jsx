41c4901bd1e825b41b25c1e65d2bb6b842ab2f73
Balthazar Gonzalez <balthagonzalez24@gmail.com>
2024-05-07 20:26:09 -0300

    fixbg

--- src/components/maincontainer/maincontainer.jsx
+++ src/components/maincontainer/maincontainer.jsx
@@ -4,6 +4,9 @@ function MainContainer({ children }) {
 
     return (
         <div className="mainContainer__wrapper">
+            <div className="stars"></div>
+            <div className="stars2"></div>
+            <div className="stars3"></div>
             <div className="mainContainer">{children}</div>
         </div>
     )
