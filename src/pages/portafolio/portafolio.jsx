1421476f4895033be7616335a0482c65fb8263fe
Balthazar Gonzalez <balthagonzalez24@gmail.com>
2024-05-24 00:25:54 -0300

    feat portafolio

--- src/pages/portafolio/portafolio.jsx
+++ src/pages/portafolio/portafolio.jsx
@@ -3,7 +3,6 @@ import Skills from '../../components/skills/skills.jsx'
 export const Portafolio = () => {
     return (
         <div className='flex-col text-center'>
-            {/* <h1 className='portafolio'>PORTAFOLIO</h1> */}
             < Skills />
         </div>
     )
